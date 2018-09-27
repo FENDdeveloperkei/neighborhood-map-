398b899b53db012843985bb1943fcdf4c93c535b
Lakeshia Williams <38958988+FENDdeveloperkei@users.noreply.github.com>
2018-09-27 12:25:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,133 +1,151 @@
 import React, { Component } from 'react';
-import Filter from './components/Filter.jsx';
 import './App.css';
+import axios from 'axios'
+import Navbar from './Navbar'
+import Hamburger from './Hamburger'
+
+window.gm_authFailure=()=>{ 
+ alert('OOPS! An Error occured while fetching GoogleMaps API! Please try again later.');
+};
+
+//variable to handle api failure
+const fourSquareFailMsg = 'OOPS! An Error occured while fetching data from FourSquare API! Please try again later.';
 
 class App extends Component {
-    state = {
-        query: '',
-        map: '',
-        markers: [],
-        infowindow: '',
-        contents: [],
-        filtered: [],
-        hideMarkers: []
-    };
+  state = {
+    venues: [],
+    markers: []
+  }
 
-    static defaultProps = {
-        locations: [
-            {name: 'The Pfield Stadium', loc: {lat: 30.4456494, lng: -97.6326804}, address: '1440 W. Pecan St, Pflugerville, TX 78660', phone: '512-594-0000', mascot: 'Pfield', wiki: 'https://en.wikipedia.org/wiki/Pflugerville_High_School'},
-            {name: 'Connally High School', loc: {lat: 30.4192234, lng: -97.6762375}, address: '13212 N Lamar Blvd, Austin, TX 78753', phone: '512-594-0800', mascot: 'Cougars', wiki: 'https://en.wikipedia.org/wiki/John_B._Connally_High_School'},
-            {name: 'Pflugerville High School',loc: {lat: 30.4448663, lng: -97.6337909}, address: '1301 W Pecan St, Pflugerville, TX 78660', phone: '512-594-0500', mascot: 'Panthers', wiki: 'https://en.wikipedia.org/wiki/Pflugerville_High_School'},
-            {name: 'Hendrickson High School',loc: {lat: 30.4656629, lng: -97.5878583}, address: '19201 Colorado Sand Dr, Pflugerville, TX 78660', phone: '512-594-1100', mascot: 'Hawks', wiki: 'https://en.wikipedia.org/wiki/Hendrickson_High_School'},
-            {name: 'Weiss High School',loc: {lat: 30.4291442, lng: -97.56574520000001}, address: '5201 Wolf Pack Dr, Pflugerville, TX 78660', phone: '512-594-1400', mascot: 'Wolves', wiki: 'https://en.wikipedia.org/wiki/Pflugerville_Independent_School_District'},
-            {name: 'Pflugerville Academic Center of Excellence',loc: {lat: 30.446406, lng: -97.63657909999999}, address: '1401-B W Pecan St, Pflugerville, TX 78660', phone: '512-594-1900', mascot: 'PACE', wiki: 'https://en.wikipedia.org/wiki/Pflugerville_Independent_School_District'}
-        ]
-    };
+  //do this right after the component is added to the DOM
+  componentDidMount(){
+    this.getVenues()//invoking getVenues function
+  }
 
-    handleFilter(query) {
-        // set state of query, setting visibility of each marker
-        this.setState({ query });
-        this.state.markers.map(marker => marker.setVisible(true));
+  updateMarkers(){
+    const listItems = document.getElementsByTagName('li');
+    const listItemsArray = Array.from(listItems);
 
-        // filter locations based on query, set state of filtered
-        if (query) {
-            const filtered = this.props.locations.filter(location => location.name.toLowerCase().includes(this.state.query.toLowerCase()));
-            this.setState({ filtered });
+    const visibleListItems = listItemsArray.filter(li=>li.offsetParent!=null);
+    const listIds = visibleListItems.map(item=>item.getAttribute('id'));
+  }
 
-            // hide markers that are not searched for and update its state
-            const hideMarkers = this.state.markers.filter(marker => filtered.every(filteredLocation => filteredLocation.name !== marker.title));
-            hideMarkers.forEach(marker => marker.setVisible(false));
-            this.setState({ hideMarkers });
-        } else {
-            this.state.markers.forEach(marker => marker.setVisible(true));
+  loadMap=()=>{
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyByxHn5EYEBHNx0XmfvEpl7AkuOlPpgM0w&callback=googleSuccess');
+    window.googleSuccess = this.googleSuccess;
+  }
+
+  getVenues=()=>{
+    //getting information from foursquare Api
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+    const parameters = {
+      client_id: '5MO2FLS4CZ0SBQJCONHJAZ4ERRGTACLX2KDMKGP5BDCSUMSE',
+      client_secret: 'JV4ESAR4GZDCTRJ5FGAXNRWB2K4UTMMB1E3VIZQ4U10YI4XR',
+      query: 'stores',
+      near: 'Nashville',
+      v: '20180925' //YYYYDDMM
+    }
+
+    //installed axios- npm install axios //axios is similar to fetch
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues: response.data.response.groups[0].items,
+      }, this.loadMap()) //calling this.loadMap() as a callback - which gets invoked after our ajax call is successful
+    })
+    .catch(err=>{
+      alert(`${fourSquareFailMsg} ${err}`)
+    })
+  }
+  
+  googleSuccess=()=>{
+
+      //creating a map
+       let myMap = new window.google.maps.Map(document.getElementById('map'), {
+          center: {lat: 36.202971, lng: -86.692699},
+          zoom: 10
+        });
+
+       const infoWindow = new window.google.maps.InfoWindow()
+
+       //looping through the venues array
+       this.state.venues.map(eachVenue => {
+        console.log(eachVenue);
+        const name = `${eachVenue.venue.name}`;
+        const address = `${eachVenue.venue.location.formattedAddress}`;
+
+        /*const imgURL = getImgURL=(id)=>{
+          const prefix = ;
+          const suffix = ;
+          const size = ;
+          const url = prefix+size+suffix
+        }*/
+
+        var contentString = `<div>
+        <img id='img'>
+        <h3>${name}</h3>
+        <p>${address}</p>
+        </div>`;
+
+        //animate marker
+        function toggleBounce(marker) {
+          marker.setAnimation(window.google.maps.Animation.BOUNCE);
+          setTimeout(function(){
+            marker.setAnimation(null);
+          }, 2000);
         }
-    }
-
-    componentDidMount() {
-        const apiKey = 'AIzaSyBuOtZ4Xw9Wo4EK96HJ5Xmrs4Rz9sv5P1c';
-        loadScript(`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
-        window.initMap = this.initMap.bind(this);
-    }
-
-    initMap() {
-        // initialize the whole map
-        const markers = [];
-        const contents = [];
-        const map = new window.google.maps.Map(document.getElementById('map'), {
-            center: {lat: 30.44837, lng: -97.6423},
-            zoom: 13,
-            mapTypeId: 'roadmap',
-            mapTypeControl: false,
-            streetViewControl: true
+        
+        // marker for each venue
+        const myMarker = new window.google.maps.Marker({
+          position: {lat: eachVenue.venue.location.lat, lng: eachVenue.venue.location.lng},
+          map: myMap,
+          title: eachVenue.venue.name,
         });
-        const infowindow = new window.google.maps.InfoWindow();
-        this.props.locations.filter(location => location.name.toLowerCase().includes(this.state.query.toLowerCase())).forEach(location => {
-            //create content string for each info window
-            const contentString = `
-                <div class="info-content">
-                    <h2>${location.name}</h2>
-                </div>
-                <p>Mascot: ${location.mascot}</p>
-                <p>Address: <a href="https://maps.google.com/?q=${location.address}">${location.address}</a></p>
-                <p>Phone: <a href="tel:${location.phone}">${location.phone}</a></p>
-                <p><a href=${location.wiki}>Check out the ${location.mascot} on Wikipedia</a></p>
-            `;
-            // create a marker for each location
-            const marker = new window.google.maps.Marker({
-                position: location.loc,
-                map: map,
-                title: location.name,
-                animation: window.google.maps.Animation.DROP
-            });
-            markers.push(marker);
-            contents.push(contentString);
-            // set the info window content to location info and open on marker click
-            marker.addListener('click', function() {
-                infowindow.setContent(contentString);
-                infowindow.open(map, marker);
-                // animate the markers on click
-                marker.setAnimation(window.google.maps.Animation.BOUNCE)
-                setTimeout(function() {
-                    marker.setAnimation(null)
-                }, 500);
-            });
-            // close info windows when map is clicked
-            map.addListener('click', function() {
-                if (infowindow) {
-                    infowindow.close();
-                }
-            })
-        });
-        this.setState({ map, markers, infowindow, contents });
-    }
 
-    render() {
-        const {query, map, markers, contents, infowindow, filtered, hideMarkers} = this.state;
-        const {locations} = this.props;
-        return (
-            <main className="app-container">
-                <header className="header">
-                    <input type="text" placeholder="Filter items" className="search" onChange={event => this.handleFilter(event.target.value)} value={query} aria-label="Filter search input" tabIndex={1} />
-                    <h1 className="title">Map of Pflugerville, TX</h1>
-                </header>
-                <Filter query={query} locations={locations} map={map} markers={markers} contents={contents} infowindow={infowindow} filtered={filtered} hideMarkers={hideMarkers} />
-                <div id="map" role="application" aria-label="map"></div>
-            </main>
-        )
-    }
+        //adding eventlistener to each marker
+        myMarker.addListener('click', function(e) {
+
+          toggleBounce(this);
+
+          //change the content
+           infoWindow.setContent(contentString)
+
+          //open an infoWindow
+          infoWindow.open(myMap, myMarker);
+
+        });
+
+        this.setState({
+          markers: [...this.state.markers, myMarker]
+       });
+
+      });
+  }
+
+  render() {
+    return (
+      <main>
+      <Navbar
+      venues = {this.state.venues}
+      map = {this.state.myMap}
+      markers = {this.state.markers}
+      changeState = {this.updateMarkers}
+      />
+      <Hamburger/>
+      <div id="map"></div>
+      </main>
+    );
+  }
 }
 
-// no libraries used, loading script tags the old fashioned way
-function loadScript(url) {
-    let index = window.document.getElementsByTagName('script')[0];
-    let script = window.document.createElement('script');
-    script.src = url;
-    script.async = true;
-    script.defer = true;
-    index.parentNode.insertBefore(script, index);
-    script.onerror = function() {
-        document.write('Error loading Map. Please try again.')
-    };
+function loadScript(url){
+  const index = window.document.getElementsByTagName('script')[0];
+  const script = window.document.createElement('script');
+  script.defer = true;
+  script.async = true;
+  script.src = url;
+  script.onerror = window.gm_authFailure;
+  index.parentNode.insertBefore(script, index);//parent.parentNode.insertBefore(child, parent);
 }
 
 export default App;
